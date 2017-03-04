856e28c9acae74d1fb2ada0bd37500df54c63e3b
mansigurung <mansigurung531@gmail.com>
2017-03-04 11:52:33 +0200

    cutipiee

---
+++ add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int  main()
+{
+	int a,b,c;
+	a=22;
+	b=32;
+	c=a+b;
+	printf("added values=%d\n",c);
+return 0;
+}
+
---
+++ factorial.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter any two numbers a,b=%d %d",a,b);
+	scanf("%d %d",&a,&b);
+	c=a*b;
+	printf("factorial=%d\n %d\n",c,c);
+return 0;
+}
